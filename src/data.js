f57202d5b7c2a5c7015a1d568929c4a9802bada4
Bradley Clifton <103290555+bjclifton@users.noreply.github.com>
2023-05-23 19:08:58 -0400

    Make mobile responsive

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ const Data = [
       date: "May 2023",
       description:
         "Gatlinburg is a charming mountain town located in Tennessee. It offers breathtaking views of the surrounding Smoky Mountains and is known for its outdoor activities, cozy cabins, and lively downtown area.",
-      link: "",
+      link: "https://goo.gl/maps/tAMGD9wzmzHSTs4K9",
       image: "https://images.unsplash.com/photo-1651773992600-794f3e226682?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1807&q=80"
     },
     {
@@ -16,7 +16,7 @@ const Data = [
       date: "Summer 2019",
       description:
         "Yellowstone National Park is a vast wilderness area located primarily in Wyoming, although it extends into Montana and Idaho as well. It is known for its geothermal features, including the famous Old Faithful geyser.",
-      link: "",
+      link: "https://goo.gl/maps/nBBjuMUfTFTfGFEb7",
       image: "https://images.unsplash.com/photo-1594073632422-ef9768f87fa4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1686&q=80"
     },
     {
@@ -26,7 +26,7 @@ const Data = [
       date: "April 2022",
       description:
         "Cancun is a popular tourist destination located on the northeastern coast of the Yucatan Peninsula in Mexico. It is known for its beautiful white sandy beaches, crystal-clear turquoise waters, and vibrant nightlife.",
-      link: "",
+      link: "https://goo.gl/maps/3r1x44vBAwCkQvd26",
       image: "https://images.unsplash.com/photo-1605217074771-4f0416bea40b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
     },
     {
@@ -36,7 +36,7 @@ const Data = [
       date: "April 2020",
       description:
         "Gulf Shores is a coastal city in Alabama, known for its stunning beaches along the Gulf of Mexico. It offers a relaxed and family-friendly atmosphere, with opportunities for swimming, sunbathing, fishing, and enjoying water sports.",
-      link: "",
+      link: "https://goo.gl/maps/DB84rxa1KHojnQkL7",
       image: "https://images.unsplash.com/photo-1586838039158-22a77e97f0a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1548&q=80"
     }
   ];
